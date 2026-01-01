74c4fb93aaeadb8dad1a1f1425965b6042982b68
Lachezar Kutsarov <lkutsarov@gmail.com>
2026-01-01 19:27:02 +0100

    Create Solution.cpp

---
+++ Solution.cpp
@@ -0,0 +1,119 @@
+
+#include <cmath>
+#include <limits>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+
+    struct NOT_POSSIBLE {
+
+        inline static const vector<string> AS_STRING_VECTOR{};
+        static const int AS_NEGATIVE_INTEGER = -1;
+        static const int AS_POSITIVE_INTEGER = numeric_limits<int>::max();
+    };
+
+    /*
+    Suffix scaffolding is </>
+     */
+    static const int SUFFIX_SCAFFOLDING_SIZE = 3;
+
+    /*
+    Complete suffix is <index/size>, index is 1-based and min possible size is 1 (when there are no splits)
+    Min split size can be one digit. Therefore, suffix must have at least two digits
+     */
+    static const int MIN_NUMBER_OF_DIGITS_IN_SUFFIX = 2;
+    static const int MIN_SUFFIX_SIZE = SUFFIX_SCAFFOLDING_SIZE + MIN_NUMBER_OF_DIGITS_IN_SUFFIX;
+
+public:
+    vector<string> splitMessage(string message, int limit) const {
+        if (limit <= MIN_SUFFIX_SIZE) {
+            return NOT_POSSIBLE::AS_STRING_VECTOR;
+        }
+
+        int totalSplits = binarySearchForMinNumberValidSplits(message, limit);
+        if (totalSplits == NOT_POSSIBLE::AS_POSITIVE_INTEGER) {
+            return NOT_POSSIBLE::AS_STRING_VECTOR;
+        }
+
+        return createSplitMessage(message, limit, totalSplits);
+    }
+
+private:
+    int binarySearchForMinNumberValidSplits(const string& message, int limit) const {
+        int minSplits = 0;
+        int maxSplits = message.length() - 1;
+        int totalSplits = NOT_POSSIBLE::AS_POSITIVE_INTEGER;
+
+        while (minSplits <= maxSplits) {
+            int numberOfSplits = minSplits + (maxSplits - minSplits) / 2;
+
+            int numberOfDigitsInTotalSplitParts = floor(log10(numberOfSplits + 1)) + 1;
+            int numberOfInputCharsInLastSplitPart = calculateNumberOfRemainingInputCharsAfterLastSplits(message.length(), limit, numberOfSplits, numberOfDigitsInTotalSplitParts);
+            int lastSuffixSize = SUFFIX_SCAFFOLDING_SIZE + 2 * numberOfDigitsInTotalSplitParts;
+            int totalSizeOfLastSplitPart = numberOfInputCharsInLastSplitPart + lastSuffixSize;
+
+            if (numberOfInputCharsInLastSplitPart > 0 && totalSizeOfLastSplitPart <= limit) {
+                totalSplits = min(numberOfSplits, totalSplits);
+
+                /*
+                Reset minSplits and maxSplits to check for valid splits less than the current number of valid splits.
+                The number of valid splits must have as few splits as possible.
+                 */
+                minSplits = 0;
+                maxSplits = numberOfSplits - 1;
+            }
+            else if (numberOfInputCharsInLastSplitPart > 0) {
+                minSplits = numberOfSplits + 1;
+            }
+            else {
+                maxSplits = numberOfSplits - 1;
+            }
+        }
+        return totalSplits;
+    }
+
+    int calculateNumberOfRemainingInputCharsAfterLastSplits(int totalInputChars, int limit, int totalSplits, int numberOfDigitsInTotalSplitParts) const {
+        int numberOfDigitsInIndex = 1;
+        int upperLimitNumberOfDigits = 1;
+
+        int remainingSplits = totalSplits;
+        int remainingInputChars = totalInputChars;
+
+        while (remainingSplits > 0) {
+            int currentSuffixSize = SUFFIX_SCAFFOLDING_SIZE + numberOfDigitsInIndex + numberOfDigitsInTotalSplitParts;
+            if (currentSuffixSize >= limit) {
+                return NOT_POSSIBLE::AS_NEGATIVE_INTEGER;
+            }
+
+            int currentNumberOfSplits = min(upperLimitNumberOfDigits * 10 - upperLimitNumberOfDigits, remainingSplits);
+            int currentRequiredChars = (limit - currentSuffixSize) * currentNumberOfSplits;
+
+            remainingSplits -= currentNumberOfSplits;
+            remainingInputChars -= currentRequiredChars;
+            upperLimitNumberOfDigits *= 10;
+            ++numberOfDigitsInIndex;
+        }
+
+        return remainingInputChars;
+    }
+
+    vector<string> createSplitMessage(const string& message, int limit, int totalSplits) const {
+        int indexResult = 0;
+        int indexStartSplit = 0;
+        int numberOfSplitMessages = totalSplits + 1;
+        vector<string> resultSplitMessage(numberOfSplitMessages);
+
+        for (int messageNumber = 1; messageNumber <= numberOfSplitMessages; ++messageNumber) {
+            string suffix = "<" + to_string(messageNumber) + "/" + to_string(numberOfSplitMessages) + ">";
+            int indexEndSplit = min(indexStartSplit + (limit - suffix.length() - 1), message.length() - 1);
+            resultSplitMessage[indexResult] = message.substr(indexStartSplit, indexEndSplit - indexStartSplit + 1) + suffix;
+            indexStartSplit = indexEndSplit + 1;
+            ++indexResult;
+        }
+
+        return resultSplitMessage;
+    }
+};
